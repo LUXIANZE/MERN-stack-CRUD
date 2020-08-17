b64a587b216fb4ee80e9aeb0513b6d8c2dc0e6a3
Lu Xianze <luxianze@Lus-iMac.realtek>
2020-08-18 00:44:19 +0800

    JSX style and further list manipulation

--- src/App.js
+++ src/App.js
@@ -11,11 +11,62 @@ function App() {
     return greeting + ", " + name + "!"
   }
 
+  
+  const courses = [
+    {
+      id: 1,
+      title: "React - The Complete Guide (incl Hooks, React Router, Redux)",
+      author: "Maximilian Schwarzmülller",
+      hours_video: 40.5,
+      number_of_lectures: 490,
+      rating: 4.6,
+      url: "https://codingthesmartway.com/courses/react-complete-guide/"
+    },
+    {
+      id: 2,
+      title: "Modern React with Redux",
+      author: "Stephen Grider",
+      hours_video: 47.5,
+      number_of_lectures: 488,
+      rating: 4.6,
+      url: "https://codingthesmartway.com/courses/modern-react-with-redux/"
+    },
+    {
+      id: 3,
+      title: "The Complete React Developer Course (w/ Hooks and Redux)",
+      author: "Andrew Mead",
+      hours_video: 39,
+      number_of_lectures: 200,
+      rating: 4.7,
+      url: "http://codingthesmartway.net/courses/complete-react-web-app-developer/"
+    }
+  ]
+
+  const wrapprStyle = {
+    padding: '100px'
+  }
+
+  const listStyle = {
+    padding: '50px 0px 0px 0px'
+  }
+
+
 
   return (
-    <div className="App">
-      <h1>{greeting}, {name}!</h1>
-      <h1>{greet(greeting, name)}</h1>
+    <div style={wrapprStyle}>
+      <h1>List of items with JSX</h1>
+      <hr></hr>
+
+      {courses.map( (course) => {
+        return (
+        <div key={course.id} style={listStyle}>
+          <h3>{course.title}</h3>
+          <span>Author: {course.author}</span>
+          <br/>
+          <span>URL: {course.url}</span>
+        </div>
+        )
+      })}
     </div>
   );
 }
